05027b2e3a593683784d7b2001cfdbac1f63114b
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-09-08 23:54:43 +0530

    JavaScript

---
+++ Day_17/Javascript_Data_Type/Data_Type.js
@@ -0,0 +1,7 @@
+console.log("Hello, World");
+console.log(2+3);
+console.log(true);
+
+console.log(typeof("Hello, World"));
+console.log(typeof(2+3));
+console.log(typeof(true));
---
+++ Day_17/Javascript_Variable/variables.js
@@ -0,0 +1,3 @@
+var myname = ("Rajeev");
+var yourname = prompt("What is your name?");
+console.log("Hello, " + myname + " and my friend name is "+ yourname);
