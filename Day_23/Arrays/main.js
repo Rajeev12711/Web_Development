ae47b3f5d2169ba13005abac1beee1f424518070
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-09-15 23:57:52 +0530

    JavaScript

---
+++ Day_23/Arrays/main.js
@@ -0,0 +1,11 @@
+var a = [10, 50, 60, 30, 70];
+console.log(a.length);
+console.log(a);
+console.log(a[3]);
+console.log(a.includes(60));
+if (a.includes(50)) {
+    console.log("Welcome");
+} else {
+    console.log("sorry");
+    
+}
