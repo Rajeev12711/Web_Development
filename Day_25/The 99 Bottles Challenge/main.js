97f8187f4d9f59fa1ae76ad740426f862e587b7d
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-09-18 23:45:05 +0530

    JavaScript

--- Day_25/The 99 Bottles Challenge/main.js
+++ Day_25/The 99 Bottles Challenge/main.js
@@ -1,13 +1,28 @@
 function bottles(){
     
     var count = 99;
+    var beerbottle = "bottles";
+
     
-    while (count != 0) {
-        console.log(count + " bottles of beer on the wall, " + count + " bottles of beer.");
-        console.log("Take one down and pass it around, " + (count-1) + " bottles of beer on the wall. \n");
+    while (count >= 0) {
+        
+        console.log(count + " " + beerbottle +  " of beer on the wall, " + count + " " + beerbottle + " of beer.");
+        if (count === 2) {
+            beerbottle = "bottle";
+        }else if (count === 1){
+            
+            count--;
+            count = "No more";
+            console.log("Take one down and pass it around, " + count + " " + beerbottle + " of beer on the wall. \n");
+
+            console.log(count + " " + beerbottle + " of beer on the wall, " + count + " " + beerbottle + " of beer.");
+            console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+
+            break;
+        } 
+        
         count--;
-    } 
-    
+        console.log("Take one down and pass it around, " + count + " " + beerbottle + " of beer on the wall. \n");       
+    }
 }
 bottles();
-
