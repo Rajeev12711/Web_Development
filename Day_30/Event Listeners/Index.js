98bdd7c0bc8cb1edbc11677b88e05cc69649c2ed
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2025-01-01 23:59:50 +0530

    JavaScript

---
+++ Day_30/Event Listeners/Index.js
@@ -0,0 +1,10 @@
+function handel() {
+    alert("I got Click.");
+}
+document.querySelectorAll("button")[0].addaEventListner("click","handel");
+document.querySelectorAll("button")[1].addaEventListner("click","handel");
+document.querySelectorAll("button")[2].addaEventListner("click","handel");
+document.querySelectorAll("button")[3].addaEventListner("click","handel");
+document.querySelectorAll("button")[4].addaEventListner("click","handel");
+document.querySelectorAll("button")[5].addaEventListner("click","handel");
+document.querySelectorAll("button")[6].addaEventListner("click","handel");
