63334c4502111600a5ffc4a214b5b9632033f393
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2025-01-14 03:21:56 +0530

    JQuery

---
+++ Day_33/JQuery/Index.js
@@ -0,0 +1,10 @@
+$("h1").css("color", "white");
+$("button").css("background-color", "red");
+$("button").css("color", "white");
+$("h1").addClass("size");
+$("button").addClass("zoom");
+$("body").append("<h1>Hello Anurag</h1>");
+
+$("h1:first").text("Rajeev");
+$("h1:eq(1)").html("<em>Bye, Anurag</em>")
+$("a").attr("href", "https://in.search.yahoo.com/?fr2=inr");
