541864d44b339a9b2b53d405592b118e72e88934
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2025-01-19 23:33:47 +0530

    JQuery

---
+++ Capstone Project 6/Simon Game/Index.js
@@ -0,0 +1,116 @@
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var started = true;
+
+var level = 0;
+
+$(document).keypress(function() {
+  
+  if (started) {
+  
+    $("h1").text("Level " + level);
+  
+    nextSequence();
+  
+    started = false;
+  }
+});
+
+$("button").on("click", handler);
+function handler(){
+
+  var userChosenColour = this.getAttribute("class");
+  
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  
+  animatePress(userChosenColour);
+  
+  checkAnswer(userClickedPattern.length -1);
+}
+
+function checkAnswer(currentLevel){
+
+  if(userClickedPattern[currentLevel] ==  gamePattern[currentLevel]){
+
+    if(userClickedPattern.length === gamePattern.length){
+  
+      setTimeout(function () {
+  
+        nextSequence();
+  
+      }, 1000);
+    }
+  
+  }else{
+    
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    
+
+    startOver();
+  }
+
+}
+
+function nextSequence() {
+  
+  userClickedPattern =[];
+  
+  level++;
+  
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour); 
+
+  
+
+  $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+}
+
+pattern = nextSequence();
+
+function playSound(name){
+  
+  var audio = new Audio("./sounds/" +name+ ".mp3");
+  
+  audio.play();
+} 
+
+function animatePress(currentColour){
+  
+  $("#" +currentColour).addClass("pressed");
+  
+  setTimeout(() => {
+  
+    $("#"+currentColour).removeClass("pressed");
+  
+  }, 100);
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = true;
+}
+
+
+
+
