7c3825bac76e176bdad95a5112a1cb2929b8d777
Rajeev <rajeev782772@gmail.com>
2025-01-29 22:37:05 +0530

    Node.js fs module

--- Day_34/Native Node Modules/Index.js
+++ Day_34/Native Node Modules/Index.js
@@ -1,11 +1,24 @@
 const fs = require("fs")
 
-// fs.writeFile("message.txt", "Hello, Native Node Modules", (err) => {
-//   if (err) throw err;
-//   console.log("The file has been Saved!.");
-// });
+fs.writeFile("message.txt", "Hello, Native Node Modules", (err) => {
+  if (err) throw err;
+  console.log("The file has been Saved!.");
+});
 
-fs.readFile("message.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+
+// To delete file in node js
+
+// fs.unlink("./message.txt",(err)=>{
+//   if(err) throw err;
+//   console.log("file is deleted");
+// // })
+fs.appendFile("message.txt",`\n Hello from Rajeev`,(err)=>{
+  if(err) throw err;
+  console.log("File appended")
+});
+
+// fs.readFile("message.txt", "utf8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   });
+ 
