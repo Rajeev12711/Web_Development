49d86dd9121eb4e595c021fbbbb835267d0740fc
Rajeev <rajeev782772@gmail.com>
2025-01-30 23:57:39 +0530

    Node.jd

--- Day_34/NPM/index.js
+++ Day_34/NPM/index.js
@@ -1,3 +1,10 @@
-var generateName = require('sillyname');
+// var generateName = require('sillyname');
+
+import generateName from "sillyname";
 var sillyName = generateName();
-console.log(`My name is ${generateName()}.`);
+console.log(`My name is ${sillyName}.`);
+
+
+
+import {randomSuperhero} from 'superheroes';
+console.log(`I am your Super Hero ${randomSuperhero()}`)
