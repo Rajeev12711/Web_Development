df49826eef3d4b28cd8a1c6671560ea11e7ca58e
Rajeev <rajeev782772@gmail.com>
2025-03-02 00:36:38 +0530

    Express

--- Day_35/Introduction to Middlewares/index4.js
+++ Day_35/Introduction to Middlewares/index4.js
@@ -1,17 +1,32 @@
 import express from "express";
-import morgan from "morgan";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandname="";
 
-app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({extended: true }));
+
+function bandnamegenerator(req, res, next){
+  console.log(req.body);
+  bandname = req.body["street"] + req.body["pet"];
+  next();
+};
+
+app.use(bandnamegenerator);
 
 app.get("/", (req, res) =>{
-  res.sendFile(req.index.html);
+  res.sendFile(__dirname + "/public/index.html");
 });
-app.post("submit/", (req, res) =>{
-  res.send("<h1>Your band name is:</h1>");
-  res.send(req.body);
+
+app.post("/submit", (req, res) =>{
+  // bandname = req.body["street"] + req.body["pet"]; we can also this way jinstead of  function bandnamegenerator.
+  res.send(`<h1>Your band name is:</h1>\n <h2> ${bandname}<h2>`);
+ 
 });
 
 app.listen(port, () => {
