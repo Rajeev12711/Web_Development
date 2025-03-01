a0743dbbd5b5a11089d25c3010964b84bd071bf3
Rajeev <rajeev782772@gmail.com>
2025-03-01 23:58:28 +0530

    Express

--- Day_35/Introduction to Middlewares/index3.js
+++ Day_35/Introduction to Middlewares/index3.js
@@ -3,10 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log("Request method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello!");
 });
 
 app.listen(port, () => {
--- Day_35/Introduction to Middlewares/index4.js
+++ Day_35/Introduction to Middlewares/index4.js
@@ -1,7 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) =>{
+  res.sendFile(req.index.html);
+});
+app.post("submit/", (req, res) =>{
+  res.send("<h1>Your band name is:</h1>");
+  res.send(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
