3ab681a6cd48a3bf1106d73e3fad9be39537e64d
Rajeev <rajeev782772@gmail.com>
2025-03-07 07:06:42 +0530

    EJS

---
+++ Day_36/EJS Tags/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+
+app.get("/", (req, res) => {
+    const data = {
+        title: "EJS Tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple", "banana", "cherry"],
+        htmlContent: "<em>This is some em text</em>",
+    };
+    res.render("index.ejs", data);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+});
